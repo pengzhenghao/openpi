9db5b716069ff3db0acdc8d0e9f758b1b1b195b4
pengzhenghao <pzh@cs.ucla.edu>
2025-12-03 13:32:27 -0800

    trying to debug log issue

--- scripts/train.py
+++ scripts/train.py
@@ -267,6 +267,12 @@ def main(config: _config.TrainConfig):
             stacked_infos = common_utils.stack_forest(infos)
             reduced_info = jax.device_get(jax.tree.map(jnp.mean, stacked_infos))
             info_str = ", ".join(f"{k}={v:.4f}" for k, v in reduced_info.items())
+
+            print(infos)
+            print(stacked_infos)
+            print(reduced_info)
+            print(info_str)
+
             pbar.write(f"Step {step}: {info_str}")
             wandb.log(reduced_info, step=step)
             infos = []
