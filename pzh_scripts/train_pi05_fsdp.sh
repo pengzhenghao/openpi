793732e6cf3b81c26dd998312fccd62addb90deb
pengzhenghao <pzh@cs.ucla.edu>
2025-12-02 23:13:30 -0800

    finalize fsdp script

--- pzh_scripts/train_pi05_fsdp.sh
+++ pzh_scripts/train_pi05_fsdp.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name="pi05_libero_fsdp"
-#SBATCH --output="pi05_libero_fsdp.out"
-#SBATCH --gres=gpu:4
-#SBATCH --ntasks-per-node=4
-#SBATCH --time=04:00:00
-#SBATCH --partition="gpu09"
+#SBATCH --job-name="pi05_libero_fsdp_%j_%t"
+#SBATCH --output="pi05_libero_fsdp_%j_%t.out"
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=7-00:00:00
+#SBATCH --partition="gpu08"
 
 # Note: no need to activate conda env
 
@@ -13,6 +13,9 @@ cd ~/vla/openpi
 
 echo "Current directory: $(pwd)"
 
-# Run the training command with FSDP enabled (4 devices)
-XLA_PYTHON_CLIENT_MEM_FRACTION=0.9 uv run scripts/train.py pi05_libero --exp-name=1202_debug_fsdp --overwrite --fsdp-devices=4
+export PYTHONUNBUFFERED=1
+export XLA_PYTHON_CLIENT_MEM_FRACTION=0.9
+
+# Run the training command with FSDP enabled (8 devices)
+uv run scripts/train.py pi05_libero --exp-name="1202_debug_fsdp_$(date +%Y%m%d_%H%M%S)" --overwrite --fsdp-devices=8
 
